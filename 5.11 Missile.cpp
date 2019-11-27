d0260b7ecc8ee1ad23e0a5304f0e4b04fb07c1f4
fzu031803146 <55052722+fzu031803146@users.noreply.github.com>
2019-11-27 13:23:57 +0800

    Add files via upload

--- 5.11 Missile.cpp
+++ 5.11 Missile.cpp
@@ -1,5 +1,30 @@
 # include <iostream>
 using namespace std;
+	int a[100002]={0},b[100002]={0};
+void q(int l,int r)
+{
+	int i=l,j=r;
+	int kk=a[(l+r)/2];
+	do
+	{
+		while (a[i]<kk) i++;
+		while (a[j]>kk) j--;
+		if (i<=j)
+		{
+			int o;
+			o=a[i];
+			a[i]=a[j];
+			a[j]=o;
+			o=b[i];
+			b[i]=b[j];
+			b[j]=o;
+			i++;
+			j--;
+		} 
+	}while (i<=j);
+	if (i<r) q(i,r);
+	if (l<j) q(l,j);
+}
 int main()
 {
 	int x1,y1,x2,y2;
@@ -7,21 +32,24 @@ int main()
 	cin>>x1>>y1>>x2>>y2;
 	int n;
 	cin>>n;
-	for (int i=0;i<n;i++)
+	for (int i=1;i<=n;i++)
 	{
 		int x,y;
 		cin>>x>>y;
-		int a,b;
-		a=(x-x1)*(x-x1)+(y-y1)*(y-y1);
-		b=(x-x2)*(x-x2)+(y-y2)*(y-y2);
-		if ((a>r1)&&(b>r2))
-		if (a>b)
+		a[i]=(x-x1)*(x-x1)+(y-y1)*(y-y1);
+		b[i]=(x-x2)*(x-x2)+(y-y2)*(y-y2);
+	}
+	q(1,n);
+	r1=a[n];
+	a[0]=0;
+	int k=r2;
+	for (int i=n;i>=0;i--)
+	{
+		if (b[i]>k) k=b[i];
+		if ((r1+r2)>(a[i-1]+k))
 		{
-			if (r2<b) r2=b;
-		}
-		else
-		{
-			if (r1<a) r1=a;
+			r1=a[i-1];
+			r2=k;
 		}
 	}
 	cout<<(r1+r2);
